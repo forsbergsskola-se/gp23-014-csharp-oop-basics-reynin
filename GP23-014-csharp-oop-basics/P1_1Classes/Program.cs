b077a5ef8624564b2dfc60e9ebe243828ba7b974
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-09 14:46:11 +0200

    P1People/Classes

---
+++ GP23-014-csharp-oop-basics/P1_1Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿Person person = new Person();
+Animal animal = new Animal();
+Car car = new Car();
+
+Console.WriteLine(person);
+Console.WriteLine(animal);
+Console.WriteLine(car);
+
+
+public class Person
+{
+    
+}
+
+public class Animal
+{
+}
+
+public class Car
+{
+}

4947aa0f6a5b0ae3c72342c3eab9875ffd15027b
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-10 11:51:38 +0200

    Update Player.cs

--- GP23-014-csharp-oop-basics/P3LevelUp/Player.cs
+++ GP23-014-csharp-oop-basics/P3LevelUp/Player.cs
@@ -1,4 +1,17 @@
 public class Player
 {
-    
+    public int Level;
+    public int Exp;
+
+    public void GrantExperience()
+    {
+        Console.WriteLine("How much Exp. do you wish to gain?");
+        Exp += int.Parse(Console.ReadLine());
+        while (Exp >= 100)
+        {
+            Level++;
+            Exp -= 100;
+        }
+        Console.WriteLine($"Lvl: {Level} Exp: {Exp}");
+    }
 }

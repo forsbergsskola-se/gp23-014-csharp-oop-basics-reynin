a32703a090dd8a11521cf2982fb17770ed91d7e0
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-11 10:20:49 +0200

    Update Program.cs

--- GP23-014-csharp-oop-basics/P4Attack/Program.cs
+++ GP23-014-csharp-oop-basics/P4Attack/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Player player1 = new Player();
+Player player2 = new Player();
 
-Console.WriteLine("Hello, World!");
+void PlayerVsPlayer(Player attacking, Player defending)
+{
+    attacking.Attack(defending);
+    Console.WriteLine($"Player 1 Damage: {player1.Damage}");
+    Console.WriteLine($"Player 2 Damage: {player2.Damage}");
+}
+
+Console.WriteLine("Player 1 attacks Player 2");
+PlayerVsPlayer(player1,player2);
+
+Console.WriteLine("Player 2 attacks Player 1");
+PlayerVsPlayer(player2,player1);
+
+Console.WriteLine("Player 1 attacks Player 1");
+PlayerVsPlayer(player1,player1);
+
+Player player3 = player1;
+
+Console.WriteLine("Player 2 attacks Player 3");
+PlayerVsPlayer(player2, player3);
+Console.WriteLine($"Player 3 Damage: {player3.Damage}");

24cec0ea032275a394a5b126d20f3db827453ca7
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-11 10:35:38 +0200

    Update Program.cs

--- GP23-014-csharp-oop-basics/Exercise/Program.cs
+++ GP23-014-csharp-oop-basics/Exercise/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+﻿/*using System.Drawing;
 
 House little = new House();
 little.color = "Turquoise";
@@ -41,3 +41,27 @@ public class Circle
 {
     public string[] roundThing = new string[4];
 }
+*/
+/*
+Person[] people = new Person [3];
+for (var i = 0; i < people.Length; i++)
+{
+    people[i] = new Person();
+    people[i].name = Console.ReadLine();
+}
+
+for (var i = 0; i < people.Length; i++)
+{
+    people[i].greeting();
+}
+
+public class Person
+{
+    public string name;
+
+    public void greeting()
+    {
+        Console.WriteLine($"Hello, I'm {this.name}!");
+    }
+}
+*/

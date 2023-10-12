6f3aacd7cb534dc86ae89bc87054e1858ea76d31
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-12 13:16:54 +0200

    P6Composition

--- GP23-014-csharp-oop-basics/P5Classified/Program.cs
+++ GP23-014-csharp-oop-basics/P5Classified/Program.cs
@@ -1,85 +1,121 @@
-﻿using System.Threading.Channels;
+﻿using Microsoft.VisualBasic;
 
-public class AblePick
-{
-    public bool IsAblePick = true;
-}
+Player player = new Player();
 
-public class Food : AblePick
+Bread bread = new Bread();
+Banana banana = new Banana();
+Apple apple = new Apple();
+Pear pear = new Pear();
+
+Sword sword = new Sword();
+Axe axe = new Axe();
+Pencil pencil = new Pencil();
+Shield shield = new Shield();
+Spear spear = new Spear();
+
+Gorilla gorilla = new Gorilla();
+Chimpanzee chimpanzee = new Chimpanzee();
+Sparrow sparrow = new Sparrow();
+Pigeon pigeon = new Pigeon();
+Hawk hawk = new Hawk();
+Tree tree = new Tree();
+
+player.Eat(bread.valueHP); //Example: Player gains HP by eating bread
+player.PowerUp(sword.valueStrength); // Example: Player gains STRENGTH by gaining a weapon 
+player.attack(gorilla.health); // Example: Player attacks by decreasing targets HEALTH with players STRENGTH
+
+
+public class Food : Player
 {
     public bool IsEdible = true;
 }
 
 public class Bread : Food
 {
-    public string bread = "Bread";
+    public int valueHP = 20;
 }
 public class Banana : Food
 {
-    public string banana = "Banana";
+    public int valueHP = 15;
 }
 public class Apple : Food
 {
-    public string apple = "Apple";
+    public int valueHP = 10;
+
 }
 public class Pear : Food
 {
-    public string pear = "pear";
+    public int valueHP = 10;
 }
 
 
-public class Weapon
+
+
+
+
+
+public class Weapon : Player
 {
     public bool isWeapon = true;
 }
 
 public class Sword : Weapon
 {
-    
+    public int valueStrength = 30;
 }
 public class Axe : Weapon
 {
-    
+    public int valueStrength = 35;
+
 }
 public class Pencil : Weapon
 {
-    
+    public int valueStrength = 1;
+
 }
-public class Shield : Weapon
+public class Shield : Weapon // Add defence points instead?
 {
-    
+    public int valueStrength = 5;
+
 }
 public class Spear : Weapon
 {
-    
+    public int valueStrength = 25;
 }
 
-public class Alive
+
+public class Object
 {
-    public bool IsAlive;
+    public bool Interactable;
 }
 
-public class Tree
+
+public class Tree : Object
 {
-    
+    public int health = 1000;
 }
-public class Gorilla : Alive
+
+public class Gorilla : Object
 {
-    
+    public int health = 100;
 }
-public class Chimpanzee : Alive
+
+public class Chimpanzee : Object
 {
-    
+    public int health = 80;
 }
-public class Sparrow : Alive
+public class Sparrow : Object
 {
-    
+    public int health = 40;
+
 }
-public class Pigeon : Alive
+public class Pigeon : Object
 {
-    
+    public int health = 10;
+
 }
-public class Hawk : Alive
+public class Hawk : Object
 {
-    
+    public int health = 50;
+}
 }

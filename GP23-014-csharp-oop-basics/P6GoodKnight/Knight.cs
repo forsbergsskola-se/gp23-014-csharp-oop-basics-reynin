24e31046c6c6fbbb0676ce051a229a29b389593b
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-12 15:53:54 +0200

    P6GoodKnight

--- GP23-014-csharp-oop-basics/P6GoodKnight/Knight.cs
+++ GP23-014-csharp-oop-basics/P6GoodKnight/Knight.cs
@@ -1,6 +1,35 @@
-namespace P6GoodKnight;
-
 public class Knight
 {
+    public Shield Defense;
+    public Sword Offense;
     
+    public void GetHit(Sword x)
+    {
+        if (x == null)
+        {
+            Console.WriteLine("Knights Shield fends off the strike.");
+        }
+        else if (x != null && Defense != null)
+        {
+            Console.WriteLine("Knights Shield was pierced by the Sword.");
+            this.Defense = null;
+        }
+        else
+        {
+            Console.WriteLine("The Knight has no Shield and is wounded.");
+        }
+        
+    }
+
+    public void Attack(Knight y)
+    {
+        if (Offense == null)
+        {
+            Console.WriteLine("Knight attacks Knight with Bare Hands.");
+        }
+        else
+        {
+            Console.WriteLine("Knight attacks Knight with a Sword.");
+        }
+    }
 }
--- GP23-014-csharp-oop-basics/P6GoodKnight/Program.cs
+++ GP23-014-csharp-oop-basics/P6GoodKnight/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Knight attacker = new Knight();
+Knight defender = new Knight();
+
+Shield shield = new Shield();
+Sword sword = new Sword();
+
+defender.Defense = shield; // Defender equip shield
+
+attacker.Attack(defender);
+defender.GetHit(attacker.Offense); // attacker.Offense == null
+
+Console.WriteLine("Equipping Sword...");
+attacker.Offense = sword;
+
+attacker.Attack(defender);
+defender.GetHit(sword);
+defender.Defense = null;
+
+attacker.Attack(defender);
+defender.GetHit(sword);
